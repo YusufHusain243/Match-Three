34b360e55e66c0caca5f0fa6b945aa597550a9bd
husainyusuf382@gmail.com <husainyusuf382@gmail.com>
2021-09-25 19:42:35 +0700

    Fixx Commit

---
+++ Assets/Scripts/PagesManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PagesManager : MonoBehaviour
+{
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    private void Update()
+    {
+        string sceneName = SceneManager.GetActiveScene().name;
+
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            if (sceneName == "Menu")
+            {
+                Application.Quit();
+            }
+            else
+            {
+                SceneManager.LoadScene("Menu");
+            }
+        }
+    }
+}
